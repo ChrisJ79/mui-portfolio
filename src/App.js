14e61d5525dd608f7fdf6693abeebc9bcef52c61
Chris <csjohnson79@icloud.com>
2020-01-15 23:31:07 -0700

    repush app.js changes

--- src/App.js
+++ src/App.js
@@ -204,7 +204,7 @@ export default function Album() {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          Github Logo
+          <img src="/github-icon__50x50.png" />
         </Typography>
       </footer>
       {/* End footer */}
